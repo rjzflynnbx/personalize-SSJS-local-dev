4b2c1dfde7159e3c0eaf5382823b21750001a44a
Richard Flynn <rif@osx-rif-n-my.local>
2023-03-02 14:23:06 +0800

    example usage comment

--- progammable.js
+++ progammable.js
@@ -4,6 +4,7 @@ const guest = loadLocalGuest('guest.json');
 
 (function () {
     //example usage
+    //you can copy & paste this code (minus lines 1 2 & 3) directly into an actual programmable, provided you load functions.js as a JS module
     var mostRecentWebSession = getMostRecentWebSession(guest);
     var sessionContainsViewEvent = sessionContainsEventOfType(mostRecentWebSession,"VIEW");
     print(sessionContainsViewEvent); // prints "true"
