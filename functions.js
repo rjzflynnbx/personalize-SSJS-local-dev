5872e58c03f8821574816da5c15d96c9c9eed590
rjzflynnbx <richard.flynn@sitecore.com>
2024-03-20 15:29:28 +0800

    more functions

--- functions.js
+++ functions.js
@@ -1,4 +1,8 @@
+// Session
 
+function getNumberOfSessions(guest) {
+    return guest.sessions.length;
+}
 
 function getMostRecentWebSession(guest) {
     var sessions = guest.sessions;
@@ -10,54 +14,11 @@ function getMostRecentWebSession(guest) {
     return null;
 }
 
-
 function getCurrentWebSession(guest) {
     var sessions = guest.sessions;
     for (var i = 0; i < sessions.length; i++) {
         if (sessions[i].sessionType === 'WEB' && sessions[i].operatingSystem != null && sessions[i].status === 'OPEN') {
             return sessions[i];
-        }
-    }
-    return null;
-}
-
-
-function getDataExtensionWithName(guest, extensionName) {
-    var toReturn = null;
-    guest.dataExtensions.forEach(function (dataExtension) {
-        if (dataExtension.name === extensionName) {
-            toReturn = dataExtension;
-        }
-    });
-    return toReturn;
-}
-
-function getDataExtensionValue(dataExtension, dataExtensionKey) {
-    if (dataExtension != null) {
-        return dataExtension.values[dataExtensionKey];
-    }
-    return null;
-}
-
-
-function getDataExtensionValue(guest, dataExtensionName, dataExtensionKey) {
-    var dataExtension = getDataExtensionWithName(guest, dataExtensionName,);
-    if (dataExtension != null) {
-        return dataExtension.values[dataExtensionKey];
-    }
-    return null;
-}
-
-function getMapFromDataExtension(dataExtension) {
-    return dataExtension.values;
-}
-
-
-function getFirstEventOfType(session, eventType) {
-    var events = session.events;
-    for (var i = 0; i < events.length; i++) {
-        if (events[i].type === eventType) {
-            return events[i];
         }
     }
     return null;
@@ -84,16 +45,63 @@ function getNumberOfEventsInSession(session, eventType) {
     return numberOfEvents;
 }
 
+function getMostRecentAbandonedSession(guest) {
+    var sessions = guest.sessions;
+    for (var i = 0; i < sessions.length; i++) {
+        if (sessions[i].sessionType === 'WEB' && sessions[i].operatingSystem != null && sessions[i].cartType === "ABANDONED") {
+            return sessions[i];
+        }
+    }
+}
+
+//Data Extension
+
+function getDataExtensionWithName(guest, extensionName) {
+    var toReturn = null;
+    guest.dataExtensions.forEach(function (dataExtension) {
+        if (dataExtension.name === extensionName) {
+            toReturn = dataExtension;
+        }
+    });
+    return toReturn;
+}
+
+function getDataExtensionValue(dataExtension, dataExtensionKey) {
+    if (dataExtension != null) {
+        return dataExtension.values[dataExtensionKey];
+    }
+    return null;
+}
+
+function getDataExtensionValue(guest, dataExtensionName, dataExtensionKey) {
+    var dataExtension = getDataExtensionWithName(guest, dataExtensionName,);
+    if (dataExtension != null) {
+        return dataExtension.values[dataExtensionKey];
+    }
+    return null;
+}
+
+function getMapFromDataExtension(dataExtension) {
+    return dataExtension.values;
+}
+
+//Events
+
+function getFirstEventOfType(session, eventType) {
+    var events = session.events;
+    for (var i = 0; i < events.length; i++) {
+        if (events[i].type === eventType) {
+            return events[i];
+        }
+    }
+    return null;
+}
 
 //TODO
 function getTotalNumberOfEvents(guest) {
     return 0;
 }
 
-
-// for a Guest
-// get all the events of type 'eventType'
-// across all sessions + devices etc.
 function getAllEventsOfType(guest, eventType) {
     return [];
 }
@@ -109,19 +117,59 @@ function getAllEventsOfType(session, eventType) {
     return eventsOfType;
 }
 
+function hasOrders(guest) {
+    return guest.orders && guest.orders.length > 0;
+}
 
-function getMostRecentAbandonedSession(guest) {
-    var sessions = guest.sessions;
-    for (var i = 0; i < sessions.length; i++) {
-        if (sessions[i].sessionType === 'WEB' && sessions[i].operatingSystem != null && sessions[i].cartType === "ABANDONED") {
-            return sessions[i];
+function getMostRecentOrder(guest) {
+    for (let i = 0; i < guest.orders.length; i++) {
+        return guest.orders[i];
+    }
+    return null;
+}
+
+//Orders
+
+function hasOrders(guest) {
+    return guest.orders && guest.orders.length > 0;
+}
+
+function getMostRecentOrder(guest) {
+    for (let i = 0; i < guest.orders.length; i++) {
+        return guest.orders[i];
+    }
+    return null;
+}
+
+function getMostRecentOrderDate(guest) {
+    const mostRecentOrder = getMostRecentOrder(guest);
+    if (mostRecentOrder) {
+        return new Date(mostRecentOrder.orderedAt);
+    }
+    return null;
+}
+
+// Segments
+
+function getSegmentMemberships(guest) {
+    const segmentMemberships = [];
+    for (let i = 0; i < guest.segmentMemberships.length; i++) {
+        segmentMemberships.push(guest.segmentMemberships[i].name);
+    }
+    return segmentMemberships;
+}
+
+function isInSegment(guest, segmentName) {
+    for (let i = 0; i < guest.segmentMemberships.length; i++) {
+        if (guest.segmentMemberships[i].name === segmentName) {
+            return true;
         }
     }
+    return false;
 }
 
 
+
 module.exports = {
-    getMostRecentWebSession, getCurrentWebSession, getDataExtensionWithName, getDataExtensionValue, getDataExtensionValue,
-    getMapFromDataExtension, getFirstEventOfType, sessionContainsEventOfType, getNumberOfEventsInSession, getTotalNumberOfEvents,
-    getAllEventsOfType, getMostRecentAbandonedSession
+    getTotalNumberOfEvents, getAllEventsOfType, getAllEventsOfType, hasOrders, getMostRecentOrder, getNumberOfSessions, getMostRecentWebSession, getCurrentWebSession, sessionContainsEventOfType, getNumberOfEventsInSession, getMostRecentAbandonedSession, getDataExtensionWithName, getDataExtensionValue, getDataExtensionValue, getMapFromDataExtension, getFirstEventOfType, hasOrders, getMostRecentOrder, getMostRecentOrderDate, getSegmentMemberships, isInSegment
 };
